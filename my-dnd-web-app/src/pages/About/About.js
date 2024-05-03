85b3207c17b65d91b0c469c10f1ac4969101b736
Moxsom <w0425436@nscc.ca>
2024-05-03 09:26:24 -0300

    base With Routing.

---
+++ my-dnd-web-app/src/pages/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+      <h1>New Page</h1>
+      <p>This is a new page in the React application.</p>
+    </div>
+    );
+    
+}
+
+export default About;
